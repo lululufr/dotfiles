0963e60040daffc9cfeeb2c91a2105cf07eaff54
lululufr <lucas.lm.miller@gmail.com>
2024-10-23 20:15:28 +0200

    first git add *

---
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,13 @@
+---@type LazySpec
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "lua",
+      "vim",
+      "markdown",
+      "markdown_inline",
+      -- add more arguments for adding more treesitter parsers
+    },
+  },
+}
