f6722431dda77a287b4d2688c23c91d0d89b9483
lululufr <lucas.lm.miller@gmail.com>
2024-10-30 10:16:56 +0100

    Ajout i3 lock

--- rofi/.config/rofi/power-menu.sh
+++ rofi/.config/rofi/power-menu.sh
@@ -5,7 +5,7 @@
 CHOSEN=$(printf "🔐 Lock\n🟠 Reboot\n🔴 Shutdown\n🖥️ Duplicate Screen\n" | rofi -dmenu)
 
 case "$CHOSEN" in
-"🔐 Lock") lockscreen ;;
+"🔐 Lock") i3lock ;;
 "🟠 Reboot") reboot ;;
 "🔴 Shutdown") poweroff ;;
 "🖥️ Duplicate Screen") xrandr --output HDMI-A-0 --mode 1920x1080 --same-as eDP ;;
