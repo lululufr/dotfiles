cc33f9128317b47d0007a550da8c3f272d558ee6
lululufr <lucas.lm.miller@proton.me>
2025-06-29 16:11:12 +0200

    cachy debut

---
+++ rofi/tmp/.config/rofi/power-menu.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Power menu script using tofi
+
+CHOSEN=$(printf "🔐 Lock\n🟠 Reboot\n🔴 Shutdown\n🖥️ Duplicate Screen\n🖥️ Extend Screen\n" | rofi -dmenu)
+
+case "$CHOSEN" in
+"🔐 Lock") i3lock ;;
+"🟠 Reboot") reboot ;;
+"🔴 Shutdown") poweroff ;;
+"🖥️ Duplicate Screen") xrandr --output HDMI-A-0 --mode 1920x1080 --same-as eDP ;;
+"🖥️ Extend Screen") xrandr --output HDMI-A-0 --auto --right-of eDP ;;
+*) exit 1 ;;
+esac
