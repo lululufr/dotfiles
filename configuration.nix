f5ebdf3808b52c4e40868372d97471001cf39392
lululufr <lucas.lm.miller@gmail.com>
2025-07-24 23:06:31 +0200

    nixos

---
+++ configuration.nix
@@ -0,0 +1,255 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, callPackage, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "tardis-nix"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Paris";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "fr_FR.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "fr_FR.UTF-8";
+    LC_IDENTIFICATION = "fr_FR.UTF-8";
+    LC_MEASUREMENT = "fr_FR.UTF-8";
+    LC_MONETARY = "fr_FR.UTF-8";
+    LC_NAME = "fr_FR.UTF-8";
+    LC_NUMERIC = "fr_FR.UTF-8";
+    LC_PAPER = "fr_FR.UTF-8";
+    LC_TELEPHONE = "fr_FR.UTF-8";
+    LC_TIME = "fr_FR.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "fr";
+    variant = "azerty";
+  };
+
+  # Configure console keymap
+  console.keyMap = "fr";
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+
+  users.users.lucas = {
+    isNormalUser = true;
+    description = "lucas";
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    packages = with pkgs; [];
+    shell = pkgs.zsh;
+
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+   	vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  	wget
+	curl
+	firefox
+	git
+	stow
+	xclip
+	alacritty
+	nodejs
+	gcc
+	zip
+	unzip
+	librewolf
+	python313
+	   # Environnement Python avec des paquets
+    	(python311.withPackages (ps: with ps; [
+      		requests
+      		pip
+    	]))
+	
+	pipx
+	killall
+	gnome-themes-extra
+	nix-template
+	#nvim package
+	neovim
+	fzf
+	lazygit
+	fd
+  ];
+
+  environment.variables = {
+	GTK_THEME = "Adwaita:dark";
+  };
+
+### experimental feature 
+
+	nix.settings.experimental-features = "nix-command flakes";
+
+### Desktop Environement
+
+
+   environment.pathsToLink = [ "/libexec" ];
+
+  services.xserver = {
+    enable = true;
+
+    desktopManager.xterm.enable = false;
+
+    windowManager.i3 = {
+      enable = true;
+      extraPackages = with pkgs; [
+        dmenu
+        polybar
+        i3status
+        rofi
+        picom
+        feh
+        dunst
+        zoxide
+        maim
+        brightnessctl
+      ];
+    };
+  };
+
+
+  services.displayManager.defaultSession = "none+i3";
+
+  programs.i3lock.enable = true;
+
+  services.picom = {
+    enable = true;
+    backend = "xrender";
+    vSync = true;
+  };
+
+### nvidia drivers	
+
+  hardware.graphics = {
+    enable = true;
+  };
+
+	services.xserver.videoDrivers = ["nvidia"];
+
+
+hardware.nvidia = {
+
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+    # of just the bare essentials.
+    powerManagement.enable = false;
+
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of 
+    # supported GPUs is at: 
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+    # Only available from driver 515.43.04+
+    open = false;
+
+    # Enable the Nvidia settings menu,
+	# accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
+
+  ### Shell
+
+
+	programs.zsh = {
+	    enable = true;
+	    enableCompletion = true;
+	    autosuggestions.enable = true;
+	    syntaxHighlighting.enable = true;
+
+	    shellAliases = {
+	      ll = "ls -l";
+	      edit = "sudo -e";
+	      update = "sudo nixos-rebuild switch";
+	      list-gen = "nixos-rebuild list-generations";
+	      del-gen = "sudo nix-collect-garbage -d && sudo nixos-rebuild switch";
+	    };
+
+	    histSize = 10000;
+	    histFile = "$HOME/.zsh_history";
+	    setOptions = [
+	      "HIST_IGNORE_ALL_DUPS"
+	    ];
+
+
+
+	    ohMyZsh = {
+	      enable = true;
+	      plugins = [
+		"git"
+		"z"
+		"history"
+	      ];
+	      theme = "robbyrussell";
+	    };
+
+	  };
+
+
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05"; # Did you read the comment?
+
+}
--- polybar/.config/polybar/forest/launch.sh
+++ polybar/.config/polybar/forest/launch.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env bash
 
-# Add this script to your wm startup file.
-
 DIR="$HOME/.config/polybar/forest"
 
-# Terminate already running bar instances
+# Kill polybar s'il tourne déjà
 killall -q polybar
 
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+# Attendre la fermeture complète
+while pgrep -u "$UID" -x polybar >/dev/null; do sleep 1; done
 
-# Launch the bar
-polybar -q main -c "$DIR"/config.ini &
+# Trouver l'écran principal
+PRIMARY=$(xrandr | grep " connected primary" | cut -d" " -f1)
+
+if [ -n "$PRIMARY" ]; then
+  echo "📺 Lancement de Polybar sur $PRIMARY"
+  MONITOR="$PRIMARY" polybar -q main -c "$DIR"/config.ini &
+else
+  echo "⚠️ Aucun écran principal trouvé. Tu dois définir un écran principal avec xrandr."
+  polybar -q main -c "$DIR"/config.ini &
+
+fi
